c3534bf087fa7ee304d14c05e52c48f0ac091d46
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-11 11:27:54 +0300

    project 1

---
+++ project/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter number A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number B: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter type of operation: ");
+string sum = Console.ReadLine();
+
+if(sum == "/")
+{
+    Console.WriteLine(a / b);
+}
+else if(sum == "+")
+{
+    Console.WriteLine(a + b);
+}
+
+else if(sum == "-")
+{
+    Console.WriteLine(a - b);
+}
+else if(sum == "*")
+{
+    Console.WriteLine(a * b);
+}
+else
+Console.WriteLine("you entered an incorrect value");
